c82a94abac903c6f6f3d21f6af61bbd981556626
Sean Abi Saab <98626715+seanabisaab@users.noreply.github.com>
2022-03-19 21:23:21 -0700

    Delete docs/Projects2 directory

--- docs/Projects2/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Calculator"},{"p":"<Unnamed>","l":"LinkedStack"},{"p":"<Unnamed>","l":"ResizableArrayStack"},{"p":"<Unnamed>","l":"StackInterface"}];updateSearchResults();
